4cbfb83272766457529ee9fc4b44c4266718c3bf
Michael Malleske <mmalleske@gmail.com>
2016-06-23 12:00:31 -0500

    some stuff

--- growler_app/routes/index.js
+++ growler_app/routes/index.js
@@ -7,18 +7,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { growls: growls });
 });
 
-router.post('/',function(req, res, next) {
-  var newGrowl = (req.body);
-  //console.log(newGrowl);
-  growls.push(newGrowl);
-  res.redirect('/');
-  console.log(growls);
+router.get('/new', function(req, res, next) {
+  res.render('/newitem/new');
 });
 
-// router.post('/', function (req, res, next){
-//   var newItem = (req.body);
-//   items.push(newItem);
-//   res.redirect('/');
-// });
+router.post('/',function(req, res, next) {
+  var newGrowl = (req.body);
 
+  growls.push(newGrowl);
+  res.redirect('/');
+});
+
+
+// function limitText(limitField, limitCount, limitNum) {
+//   if (limitField.value.length > limitNum) {
+//     limitField.value = limitField.value.substring(0, limitNum);
+//   } else {
+//     limitCount.value = limitNum - limitField.value.length;
+//   }
+// }
+function limitText(){
+  console.log('Hello');
+}
 module.exports = router;
